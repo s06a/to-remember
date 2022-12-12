c48686010a6e37b989374e54c8ec3879dd1105d7
s06a <91134347+s06a@users.noreply.github.com>
2022-12-12 19:57:57 +0330

    added second shell script question

---
+++ shell-scripting/reverse_command_line_argument.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Question
+<< 'COMMENT'
+get a name as a command line argument, reverse it, and say hello reversed!
+COMMENT
+
+#Answer
+var="$1"
+for (( i=${#var}; i>=0; i-- ))
+do
+	rev="$rev${var:$i:1}"
+done
+
+echo "Hello $rev"
