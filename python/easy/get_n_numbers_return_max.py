7d8ca25b9e6a5548932fb4cf6dc588399e952450
s06a <91134347+s06a@users.noreply.github.com>
2022-12-13 23:35:28 +0330

    fixed

--- python/easy/get_n_numbers_return_max.py
+++ python/easy/get_n_numbers_return_max.py
@@ -4,8 +4,10 @@ get n numbers from input (separated by space) and return the maximum
 '''
 
 # Answer
-def find_maximum():
-    numbers = input().split(' ')
+def find_maximum(numbers):
 	list = [int(num) for num in numbers] 
 	return max(list)
+
+numbers = input().split(' ')
+find_maximum(numbers)
 
