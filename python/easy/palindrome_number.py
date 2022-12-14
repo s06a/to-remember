76579ec2ca8454795b7c20208f5b61b3ede20132
s06a <91134347+s06a@users.noreply.github.com>
2022-12-14 19:03:50 +0330

    fixed syntax issue in palindorme number function

--- python/easy/palindrome_number.py
+++ python/easy/palindrome_number.py
@@ -6,7 +6,7 @@ check if the number is palindorme; it's equal to its reverse
 # Answer
 def palindrome_checker(number):
     number = str(number)
-    if number == number[::-1]
+    if number == number[::-1]:
         return True
     else:
         return False
