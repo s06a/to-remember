2dc0b184002ef0085b261a2fa30f86bdd45d6c86
s06a <91134347+s06a@users.noreply.github.com>
2022-12-14 13:35:24 +0330

    added python question: factorial calculator

---
+++ python/easy/factorial_calculator.py
@@ -0,0 +1,15 @@
+# Question
+'''
+get an integer and return its factorial
+'''
+
+# Answer
+def factorial(n):
+    sum = 1
+    while n>0:
+        sum *= n
+        n -= 1
+    return sum
+
+n = int(input())
+print(factorial(n))
