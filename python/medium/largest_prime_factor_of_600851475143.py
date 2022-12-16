ed45734bd1c3dbb309a1b7ebfac9571f7f305326
s06a <91134347+s06a@users.noreply.github.com>
2022-12-16 23:16:30 +0330

    changed is_prime function with a more efficient prime checker

--- python/medium/largest_prime_factor_of_600851475143.py
+++ python/medium/largest_prime_factor_of_600851475143.py
@@ -8,7 +8,10 @@ def is_prime(n):
     returns True if n is prime
     and False if n is not prime"""
 
-    for i in range(2, int(n**0.5) + 1):
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
 
@@ -18,10 +21,14 @@ def largest_prime_factor(n):
     """n is integer
     returns largest prime factor of n"""
     prime_factors = []
+    step = 1
 
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n**0.5) + 1, step):
         if is_prime(i) and n % i == 0:
             prime_factors.append(i)
+
+        if i == 2:
+            step += 1
 
     if len(prime_factors):
         return max(prime_factors)
