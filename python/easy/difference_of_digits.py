f1bf3807cc709c084de9564c13a64f37276b920d
s06a <91134347+s06a@users.noreply.github.com>
2022-12-16 14:29:01 +0330

    fixed pep8 and name == main

--- python/easy/difference_of_digits.py
+++ python/easy/difference_of_digits.py
@@ -1,12 +1,15 @@
 # Question
-'''
-get a two digit number, return the difference between the larger digit and the smaller digit
-'''
+"""get a two digit number, return the difference 
+between the larger digit and the smaller digit"""
 
 # Answer
 def calculate_difference(number):
+    """number is a two digits integer
+    returns the difference between the 
+    larger digit and the smaller digit"""
     number = [int(n) for n in number]
     return max(number)-min(number)
 
-number = input()
-calculate_difference(number)
+if __name__ == "__main__":
+    number = input()
+    calculate_difference(number)
