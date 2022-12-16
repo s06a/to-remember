ee65fcd65142c6da4116c597389f1d601c4af9c2
s06a <91134347+s06a@users.noreply.github.com>
2022-12-16 22:57:16 +0330

    added question: project euler 3

--- python/medium/largest_prime_factor_of_600851475143.py
+++ python/medium/largest_prime_factor_of_600851475143.py
@@ -4,6 +4,10 @@ the number 600851475143?"""
 
 # Answer
 def is_prime(n):
+    """n is integer
+    returns True if n is prime
+    and False if n is not prime"""
+
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
@@ -11,6 +15,8 @@ def is_prime(n):
     return True
 
 def largest_prime_factor(n):
+    """n is integer
+    returns largest prime factor of n"""
     prime_factors = []
 
     for i in range(2, int(n**0.5) + 1):
