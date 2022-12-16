08ba3855a91a75bc6a35b94ac05dba825c81d2f4
s06a <91134347+s06a@users.noreply.github.com>
2022-12-16 14:35:20 +0330

    fixed pep8 and name == main

--- python/easy/get_n_numbers_return_max.py
+++ python/easy/get_n_numbers_return_max.py
@@ -1,13 +1,15 @@
 # Question
-'''
-get n numbers from input (separated by space) and return the maximum
-'''
+"""get n numbers from input (separated by space) 
+and return the maximum"""
 
 # Answer
 def find_maximum(numbers):
+    """numbers is a space separated sequence
+    returns maximum of the sequence"""
 	list = [int(num) for num in numbers] 
 	return max(list)
 
-numbers = input().split(' ')
-find_maximum(numbers)
+if __name__ == "__main__":
+    numbers = input().split(' ')
+    find_maximum(numbers)
 
