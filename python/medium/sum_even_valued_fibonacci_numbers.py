296322ad5a94c6436ac9ef3684463f18806c60dd
s06a <91134347+s06a@users.noreply.github.com>
2022-12-16 21:06:25 +0330

    added question: project euler 2

---
+++ python/medium/sum_even_valued_fibonacci_numbers.py
@@ -0,0 +1,22 @@
+# Question
+"""By considering the terms in the Fibonacci sequence
+whose values do not exceed four million, find the sum
+of the even-valued terms
+source: https://projecteuler.net/problem=2"""
+
+# Answer
+def is_even(n):
+    """n is integer
+    returns True or False"""
+
+    return n % 2 == 0
+
+if __name__ == "__main__":
+    i, j = 1, 1
+    sum = 0
+
+    while j < 4*10**6:
+        if is_even(j):
+            sum += j
+        i, j = j, i + j
+    print(sum)
