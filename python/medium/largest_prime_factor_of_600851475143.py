5b19c278d6b5035c006d8b9e30913671b8eb28db
s06a <91134347+s06a@users.noreply.github.com>
2022-12-18 00:56:14 +0330

    changed algorithm again, this one is better as a coding challenge

--- python/medium/largest_prime_factor_of_600851475143.py
+++ python/medium/largest_prime_factor_of_600851475143.py
@@ -2,25 +2,50 @@
 """What is the largest prime factor of the number 600851475143?
 source: https://projecteuler.net/problem=3
 """
+"""not the fastest algorithm, however, for this question it's
+still pretty fast
+"""
 
 # Answer
 
 
-def largest_prime_factor(number):
+def is_prime(n):
+    """n is integer
+    returns True if n is prime
+    and False if n is not prime
+    """
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    for i in range(5, int(n**0.5) + 1, 6):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def largest_prime_factor(n):
     """n is integer
     returns largest prime factor of n
-    source: https://codereview.stackexchange.com/a/276811
     """
-    factor = 2
-    remnant = number
+    prime_factors = []
+    step = 1
 
-    while factor**2 <= remnant:
-        if remnant % factor == 0:
-            remnant //= factor
-        else:
-            factor += 1
+    for i in range(2, int(n**0.5) + 1, step):
+        if is_prime(i) and n % i == 0:
+            prime_factors.append(i)
 
-    return remnant
+        if i == 2:
+            step += 1
+
+        if i == 5:
+            step += 4
+
+    if len(prime_factors):
+        return max(prime_factors)
+    else:
+        return n
 
 
 if __name__ == "__main__":
