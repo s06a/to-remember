bce365fc1bd15970575363e72fdb22af30971419
s06a <91134347+s06a@users.noreply.github.com>
2022-12-17 12:26:25 +0330

    added question: min_max sequence

---
+++ python/easy/max_min_max_min.py
@@ -0,0 +1,29 @@
+# Question
+"""read a sequence of numbers, if it's length is
+even return maximum and delete the element, if it's
+length is odd return minimum and delete the element,
+do this till the end"""
+
+# Answer
+def return_max_min(sequence):
+    """sequence is list of integers
+    returns maximum if length of the sequence
+    is even, and returns minimum if the length
+    of the sequence is odd"""
+
+    if len(sequence) % 2 == 0:
+        maximum = max(sequence)
+        sequence = sequence.pop(sequence.index(maximum))
+
+        return maximum
+    else:
+        minimum = min(sequence)
+        sequence = sequence.pop(sequence.index(minimum))
+
+        return minimum
+
+if __name__ == "__main__":
+    sequence = [int(n) for n in input().split(' ')]
+
+    while sequence:
+        print(return_max_min(sequence))
