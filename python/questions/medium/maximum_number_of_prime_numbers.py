9c3af0991b0150f2fa1f357f36c6f1e571b8eb6d
s06a <91134347+s06a@users.noreply.github.com>
2022-12-23 16:43:54 +0330

    added python question: maximum number of prime numbers

---
+++ python/questions/medium/maximum_number_of_prime_numbers.py
@@ -0,0 +1,33 @@
+# Question
+"""get a list of numbers, return the number which 
+has maximum number of prime factors
+"""
+
+# Answer
+
+
+def number_of_prime_factors(n):
+    """Returns number of prime factors for a number.
+
+    n is integer
+    returns integer
+    """
+    factor = 2
+    remnant = n
+    num = 0
+
+    while factor <= remnant:
+        if remnant % factor == 0:
+            remnant //= factor
+            num += 1
+        else:
+            factor += 1
+
+    return num
+
+
+if __name__ == "__main__":
+
+    list = [40, 54564654, 1321321, 121]
+
+    print(max((n, number_of_prime_factors(n)) for n in list)[0])
